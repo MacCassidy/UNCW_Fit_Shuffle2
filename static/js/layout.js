31f40399c9348a178227c612165dec7d3598e725
MacCassidy <CormacQCassidy@gmail.com>
2020-04-24 00:26:03 -0400

    ayy fixed it i wthink

--- static/js/layout.js
+++ static/js/layout.js
@@ -1,4 +1,8 @@
-var window_height = window.innerHeight;
-var window_width = window.innerWidth;
-$('body').css('height', (parseInt(window_height) + 'px'));
-$('body').css('width', (parseInt(window_width) + 'px'));
+$(document).ready(function(){
+  var window_height = window.innerHeight;
+  console.log(window_height);
+  var window_width = window.innerWidth;
+  console.log(window_width);
+  $('body').css('height', (parseInt(window_height) + 'px'));
+  $('body').css('width', (parseInt(window_width) + 'px'));
+});
