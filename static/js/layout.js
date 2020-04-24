daa9dfb71ee23408057dd997c7c312a832cdfb3a
MacCassidy <CormacQCassidy@gmail.com>
2020-04-23 23:30:54 -0400

    trying out the window.innerHeight trick

--- static/js/layout.js
+++ static/js/layout.js
@@ -1,4 +1,4 @@
 var window_height = window.innerHeight;
 var window_width = window.innerWidth;
-$('body').css('height', parseInt(window_height));
-$('body').css('width', parseInt(window_width));
+$('body').css('height', (parseInt(window_height) + 'px'));
+$('body').css('width', (parseInt(window_width) + 'px'));
