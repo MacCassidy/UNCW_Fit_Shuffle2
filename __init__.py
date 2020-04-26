7327f3770454f7fc350bdcc473f7726dc9b2bb24
MacCassidy <CormacQCassidy@gmail.com>
2020-04-26 02:50:11 -0400

    dsfjhhskj

--- __init__.py
+++ __init__.py
@@ -9,25 +9,43 @@ import re
 import bcrypt
 import smtplib, ssl, certifi
 from validate_email import validate_email
+from flask_apscheduler import APScheduler
+import random
+# import mysql.connector
+# from apscheduler.schedulers.background import BackgroundScheduler
+
+
+# scheduler = BackgroundScheduler()
+# job = scheduler.add_job(test_job, 'interval', minutes=1)
+# scheduler.start()
+
 
 app = Flask(__name__)
+# scheduler = APScheduler()
+# scheduler.init_app(app)
+# scheduler.add_job(id='Scheduled task', func = registertask, trigger='interval', seconds=7)
+# scheduler.start()
 
 # Browser Caching turned off
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-
+app.config['SCHEDULER_API_ENABLED'] = True
 # MySql config
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'tempuser1'
 app.config['MYSQL_PASSWORD'] = 'Meenoo737!'
 app.config['MYSQL_DB'] = 'tempdb1'
 app.config['MYSQL_PORT'] = 3306
-mysql = MySQL(app)
-# mysql.connection.commit()
-# cur.close()
-
-#Session config
 app.secret_key = 'my_key'
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes = 20)
+mysql = MySQL(app)
+scheduler = APScheduler()
+# mysql.connection.commit()
+# cur.close()
+scheduler.init_app(app)
+
+#Session config
+# app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes = 20)
+# mysql = MySQL(app)
 # app.config['SESSION_COOKIE_SECURE'] = True
 # flask.session.modified = True
 # flask.session.permanent = True
@@ -64,6 +82,8 @@ def homepage():
         email = account['email']
         username = account['username']
         cursor.close()
+        scheduler.add_job(id='Scheduledtask', func = registertask,  trigger='interval', seconds=7)
+        scheduler.start()
         return render_template('homepage.html', email=email, username=username)
     else:
         return redirect(url_for('gateway'))
@@ -128,16 +148,39 @@ def logout():
         return redirect(url_for('gateway'))
 
 
-# @app.route('/register', methods=['POST'])
-# def register():
-#     username = request.form['username']
-#     email = request.form['email']
-#     password = request.form['password']
-#     age = request.form['age']
-#     height_ft = request.form['age']
-# @app.route('/logout', methods=['POST'])
-# def logout():
-#     return redirect(url_for('gateway'))
+
+@app.route('/register', methods=['POST'])
+def register():
+    if 'username' in request.form and 'email' in request.form and 'password' in request.form and 'age' in request.form and 'height_ft' in request.form and 'height_in' in request.form and 'gender' in request.form and 'timezone' in request.form and 'exp_cardio' in request.form and 'exp_chest' in request.form and 'exp_legs' in request.form and 'exp_back' in request.form and 'exp_core' in request.form and 'exp_shoulders' in request.form and 'exp_arms' in request.form:
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        age = request.form['age']
+        height_ft = request.form['height_ft']
+        height_in = request.form['height_in']
+        gender = request.form['gender']
+        timezone = request.form['timezone']
+        exp_cardio = request.form['exp_cardio']
+        exp_chest = request.form['exp_chest']
+        exp_legs = request.form['exp_legs']
+        exp_back = request.form['exp_back']
+        exp_core = request.form['exp_core']
+        exp_shoulders = request.form['exp_shoulders']
+        exp_arms = request.form['exp_arms']
+        if username and email and password and age and height_ft and height_in and gender and timezone and exp_cardio and exp_chest and exp_legs and exp_back and exp_core and exp_shoulders and exp_arms:
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute("SELECT * FROM Accounts WHERE email = %(email)s and account_id = %(account_id)s", {'email': email, 'account_id': account_id})
+
+            # mysql.connection.commit()
+            # cursor.close()
+            # session.permanent = True
+            # session['logged_in'] = True
+            # session['email'] = account['email']
+            # session['account_id'] = account['account_id']
+        else:
+            return jsonify({'error' : 'missing data'})
+    else:
+        return jsonify({'error' : 'missing data'})
 
 
 
@@ -160,7 +203,87 @@ def logout():
 #     r.headers['Cache-Control'] = 'public, max-age=0'
 #     return r
 
+# username VARCHAR(25) NOT NULL,
+# email VARCHAR(50) NOT NULL UNIQUE,
+# password VARCHAR(60) NOT NULL,
+# logged_in BOOLEAN NOT NULL,
+# age TINYINT NOT NULL check(age > 13 and age < 65),
+# height_in TINYINT NOT NULL CHECK(height_in >= 0 and height_in < 12),
+# height_ft TINYINT NOT NULL CHECK(height_ft >= 3 and height_ft <= 8),
+# gender  NOT NULL CHECK(gender IN ('male', 'female')),
+# timezone  NOT NULL CHECK(timezone IN ('America/New_York', 'America/Merida', 'America/Boise', 'America/Dawson', 'America/Anchorage', 'Pacific/Honolulu', 'America/Phoenix')),
+# exp_cardio  NOT NULL CHECK(exp_cardio IN ('beginner', 'novice', 'intermediate', 'experienced')),
+# exp_chest  NOT NULL CHECK(exp_chest IN ('beginner', 'novice', 'intermediate', 'experienced')),
+# exp_legs  NOT NULL CHECK(exp_legs IN ('beginner', 'novice', 'intermediate', 'experienced')),
+# exp_back VARCHAR(12) NOT NULL CHECK(exp_back IN ('beginner', 'novice', 'intermediate', 'experienced')),
+# exp_core VARCHAR(12) NOT NULL CHECK(exp_core IN ('beginner', 'novice', 'intermediate', 'experienced')),
+# exp_shoulders VARCHAR(12) NOT NULL CHECK(exp_shoulders IN ('beginner', 'novice', 'intermediate', 'experienced')),
+# exp_arms VARCHAR(12) NOT NULL CHECK(exp_arms IN ('beginner', 'novice', 'intermediate', 'experienced')),
+# password_change_code INT UNIQUE CHECK(password_change_code <= 10000000 and password_change_code >= 99999999),
+# created DECIMAL(16,6) NOT NULL,
+# last_activity DECIMAL(16,6) NOT NULL,
+
+
+
+def registertask():
+    with scheduler.app.app_context():
+        # from app import app
+        # print(mysql.connection)
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        if cursor:
+            print("yea suuuuuun")
+    # cursor.close()
+    # import mysql.connector
+
+    # mydb = mysql.connector.connect(
+    #   host="localhost",
+    #   user="tempuser1",
+    #   passwd="Meenoo737!",
+    #   database="tempdb1",
+    #   auth_plugin='mysql_native_password'
+    # )
+    # mycursor = mydb.cursor()
+    #
+    #
+    #
+    #
+        username = "WakaWakaaWaka"
+        r1 = random.randint(0, 10000)
+        email = "task" + str(r1) + "@mail.com"
+        # colepassword
+        password = '$2b$12$LaczxaUdSOiyRbC/jyRfkufezVXhoCllWfGVYRB4xyKOBQoC/jCMS'
+        logged_in = '0'
+        age = "15"
+        height_ft = "5"
+        height_in = "5"
+        gender = "male"
+        timezone = 'America/New_York'
+        exp_cardio = 'novice'
+        exp_chest = 'novice'
+        exp_legs = 'novice'
+        exp_back = 'novice'
+        exp_core = 'novice'
+        exp_shoulders = 'novice'
+        exp_arms = 'novice'
+        created = str(datetime.now().timestamp())
+        last_activity = str(datetime.now().timestamp())
+    # cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    # cursor.execute("SELECT * FROM Accounts WHERE email = %(email)s and account_id = %(account_id)s", {'email': email, 'account_id': account_id})
+    # mycursor.execute("CREATE DATABASE mydatabase")
+        cursor.execute("INSERT INTO Accounts (username,email,password,logged_in,age,height_ft,height_in,gender,timezone, exp_cardio, exp_chest, exp_legs, exp_back, exp_core, exp_shoulders, exp_arms, created, last_activity  ) VALUES (  %(username)s, %(email)s, %(password)s, %(logged_in)s, %(age)s, %(height_ft)s, %(height_in)s, %(gender)s, %(timezone)s, %(exp_cardio)s, %(exp_chest)s, %(exp_legs)s, %(exp_back)s, %(exp_core)s, %(exp_shoulders)s, %(exp_arms)s, %(created)s, %(last_activity)s )", {'username': username, 'email': email,  'password': password, 'logged_in': logged_in, 'age': age, 'height_ft': height_ft, 'height_in': height_in, 'gender': gender, 'timezone': timezone, 'exp_cardio':exp_cardio, 'exp_chest': exp_chest, 'exp_legs': exp_legs, 'exp_back': exp_back, 'exp_core': exp_core, 'exp_shoulders': exp_shoulders, 'exp_arms': exp_arms, 'created': created, 'last_activity': last_activity})
+    # mydb.commit()
+    # mydb.close()
+        mysql.connection.commit()
+        cursor.close()
+    # {'username': username, 'email': email,  'password': password, 'logged_in': logged_in, 'age': age, 'height_ft': height_ft, 'height_in': height_in, 'gender': gender, 'timezone':
+    # timezone, 'exp_cardio':exp_cardio, 'exp_chest': exp_chest, 'exp_legs': exp_legs, 'exp_back': exp_back, 'exp_core': exp_core, 'exp_shoulders': exp_shoulders, 'exp_arms': exp_arms}
+
 if __name__ == "__main__":
-    # app.run(debug = True)
-    # app.run(host="localhost", port=8000, debug=True)
+    # scheduler = APScheduler()
+    # scheduler.init_app(app)
+    # scheduler.add_job(id='Scheduledtask', func = registertask,  trigger='interval', seconds=7)
+    # scheduler.init_app(app)
+    # scheduler.add_job(id='Scheduledtask', func = registertask,  trigger='interval', seconds=7)
+    # scheduler.init_app(app)
+    # scheduler.start()
     app.run()
