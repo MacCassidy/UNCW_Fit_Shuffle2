b0bb84b0d5462b6f5920456c0e70bd4cde832a31
MacCassidy <CormacQCassidy@gmail.com>
2020-05-03 23:33:50 -0400

    234234

--- __init__.py
+++ __init__.py
@@ -231,24 +231,24 @@ def register():
                         code: %s
                     """ % (reg_code)
                     context = ssl.create_default_context()
-                    try:
-                        print('SMTP try block #$%$#%$%%#%#$%$#%#%#$%#$%$#')
-                        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as server:
-                            server.login(our_email, password)
-                            server.sendmail(our_email, rec_email, message)
-
-                    except Exception as e:
-                        print(e)
-                        return jsonify({'error' : 'missing data'})
+                    # try:
+                    #     print('SMTP try block #$%$#%$%%#%#$%$#%#%#$%#$%$#')
+                    #     with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as server:
+                    #         server.login(our_email, password)
+                    #         server.sendmail(our_email, rec_email, message)
+                    #
+                    # except Exception as e:
+                    #     print(e)
+                    #     return jsonify({'error' : 'missing data'})
                     # harambe
                     cur_var = str(datetime.now() + timedelta(seconds=30))[:19]
                     seed = random.randint(1000, 9999)
                     sched_id = 'Registertask-' + account['email'] + "-" + str(seed)
                     print("scheduler job #$%#%#$%#$%#$%#$%#%")
-                    # try:
-                    #     scheduler.add_job(name="RegisterTask", id=sched_id, func = registercheck,  trigger='date', run_date=cur_var, kwargs = { 'u_id': str(account['account_id']), 'email': str(account['email']), 'created': str(created_stamp), 'code': str(account['register_code'])} )
-                    # except Exception as e:
-                    #     print(e)
+                    try:
+                        scheduler.add_job(name="RegisterTask", id=sched_id, func = registercheck,  trigger='date', run_date=cur_var, kwargs = { 'u_id': str(account['account_id']), 'email': str(account['email']), 'created': str(created_stamp), 'code': str(account['register_code'])} )
+                    except Exception as e:
+                        print(e)
                     print("return none$#$$$$$$$$")
                     return jsonify({'error' : 'none'})
         else:
